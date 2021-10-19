cc14717bf3d2284d7d465c311d63e04b6af3d384
Auroberfroid <aurelien.roberfroid@gmail.com>
2021-10-19 15:37:45 +0300

    added the user info tab when clicking on the user picture (task 7)

--- res/js/app.js
+++ res/js/app.js
@@ -1,20 +1,20 @@
-$(function () {
+$(function() {
     function getPosts() {
-      return $.get({
-        url: "https://api.jsonbin.io/b/616bf937aa02be1d445a969d/6",
-        success: function (response) {
-          return response;
-        },
-        error: function (e) {
-          alert(`error retrieving posts: ${JSON.stringify(e)}`);
-        },
-      });
+        return $.get({
+            url: "https://api.jsonbin.io/b/616bf937aa02be1d445a969d/6",
+            success: function(response) {
+                return response;
+            },
+            error: function(e) {
+                alert(`error retrieving posts: ${JSON.stringify(e)}`);
+            },
+        });
     }
 
     getPosts()
-      .then((res) => {
-        for (let post of res) {
-            $(".container").append(`
+        .then((res) => {
+            for (let post of res) {
+                $(".container").append(`
             <div class="post">
                 <div class="post-header">
                     <img src="${post.author.photo_profile}">
@@ -25,10 +25,20 @@ $(function () {
                 <input type="image" src="res/images/likebutton.png" alt="like_button">
             </div>
             `);
-            
-        }
-      })
-      .catch(function (e) {
-        alert(e);
-      });
-  });
+            }
+        })
+
+        .catch(function(e) {
+            alert(e);
+        });
+
+        $("#logo_profile").click(function(){
+            let userDisplay = $("#userDisplay");
+            if (userDisplay.is(":visible") == false){
+                userDisplay.show();
+            }
+            else {
+                userDisplay.hide();
+            }
+            });
+    });
