8b96ab5424c0a7633f5e877417e524f520b1bd7d
janefe <janfendioglu@hotmail.com>
2021-10-24 15:50:25 +0300

    Comments

--- res/js/app.js
+++ res/js/app.js
@@ -1,36 +1,46 @@
 // Retrieve posts information from "JSONbin.io" 
 $(function () {
-    function getPosts() {
+  function getPosts() {
       return $.get({
-        url: "https://api.jsonbin.io/b/616bf937aa02be1d445a969d/6",
-        success: function (response) {
-          return response;
-        },
-        error: function (e) {
-          alert(`error retrieving posts: ${JSON.stringify(e)}`);
-        },
+          url: "https://api.jsonbin.io/b/616bf937aa02be1d445a969d/6",
+          success: function(response) {
+              return response;
+          },
+          error: function(e) {
+              alert(`error retrieving posts: ${JSON.stringify(e)}`);
+          },
       });
-    }
+  }
 
-    getPosts()
-      .then((res) => {
-        for (let post of res) {
-          //Dynamically fills the posts section
-            $(".container").append(`
-            <div class="post">
-                <div class="post-header">
-                    <img src="${post.author.photo_profile}">
-                    <small>${post.create_date}</small>
-                </div>
-                <img src="${post.image}" class="image">
-                <p>${post.description}</p>
-                <input type="image" src="res/images/likebutton.png" alt="like_button">
-            </div>
-            `);
-            
-        }
+  getPosts()
+    .then((res) => {
+      for (let post of res) {
+        //Dynamically fills the posts section
+          $(".container").append(`
+          <div class="post">
+              <div class="post-header">
+                  <img src="${post.author.photo_profile}">
+                  <small>${post.create_date}</small>
+              </div>
+              <img src="${post.image}" class="image">
+              <p>${post.description}</p>
+              <input type="image" src="res/images/likebutton.png" alt="like_button">
+          </div>
+          `);
+          }
       })
-      .catch(function (e) {
-        alert(e);
+
+      .catch(function(e) {
+          alert(e);
       });
+
+      $("#logo_profile").click(function(){
+          let userDisplay = $("#userDisplay");
+          if (userDisplay.is(":visible") == false){
+              userDisplay.show();
+          }
+          else {
+              userDisplay.hide();
+          }
+          });
   });
